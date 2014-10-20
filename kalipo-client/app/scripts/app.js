082582ad65e3896f8af3ae6adc4f3a5cb4a99b4c
damoeb <maggo.ruepp@gmail.com>
2014-10-20 20:19:14 +0200

    deactivate atmosphere

--- kalipo-client/app/scripts/app.js
+++ kalipo-client/app/scripts/app.js
@@ -177,54 +177,55 @@ kalipoApp
             $location.path('');
         });
     })
-    .run(function ($rootScope, $route) {
-        // This uses the Atmoshpere framework to do a Websocket connection with the server, in order to send
-        // user activities each time a route changes.
-        // The user activities can then be monitored by an administrator, see the views/tracker.html Angular view.
-
-        $rootScope.websocketSocket = atmosphere;
-        $rootScope.websocketSubSocket;
-        $rootScope.websocketTransport = 'websocket';
-
-        $rootScope.websocketRequest = { url: 'websocket/activity',
-            contentType: "application/json",
-            transport: $rootScope.websocketTransport,
-            trackMessageLength: true,
-            reconnectInterval: 5000,
-            enableXDR: true,
-            timeout: 60000 };
-
-        $rootScope.websocketRequest.onOpen = function (response) {
-            $rootScope.websocketTransport = response.transport;
-            $rootScope.websocketRequest.sendMessage();
-        };
-
-        $rootScope.websocketRequest.onClientTimeout = function (r) {
-            $rootScope.websocketRequest.sendMessage();
-            setTimeout(function () {
-                $rootScope.websocketSubSocket = $rootScope.websocketSocket.subscribe($rootScope.websocketRequest);
-            }, $rootScope.websocketRequest.reconnectInterval);
-        };
-
-        $rootScope.websocketRequest.onClose = function (response) {
-            if ($rootScope.websocketSubSocket) {
-                $rootScope.websocketRequest.sendMessage();
-            }
-        };
-
-        $rootScope.websocketRequest.sendMessage = function () {
-            if ($rootScope.websocketSubSocket.request.isOpen) {
-                $rootScope.websocketSubSocket.push(atmosphere.util.stringifyJSON({
-                        userLogin: $rootScope.login,
-                        page: $route.current.templateUrl}
-                ));
-            }
-        };
-
-        $rootScope.websocketSubSocket = $rootScope.websocketSocket.subscribe($rootScope.websocketRequest);
-
-        $rootScope.$on("$routeChangeSuccess", function (event, next, current) {
-            $rootScope.websocketRequest.sendMessage();
-        });
-    }
-);
+//    .run(function ($rootScope, $route) {
+//      todo activate atmosphere
+//        // This uses the Atmoshpere framework to do a Websocket connection with the server, in order to send
+//        // user activities each time a route changes.
+//        // The user activities can then be monitored by an administrator, see the views/tracker.html Angular view.
+//
+//        $rootScope.websocketSocket = atmosphere;
+//        $rootScope.websocketSubSocket;
+//        $rootScope.websocketTransport = 'websocket';
+//
+//        $rootScope.websocketRequest = { url: 'websocket/activity',
+//            contentType: "application/json",
+//            transport: $rootScope.websocketTransport,
+//            trackMessageLength: true,
+//            reconnectInterval: 5000,
+//            enableXDR: true,
+//            timeout: 60000 };
+//
+//        $rootScope.websocketRequest.onOpen = function (response) {
+//            $rootScope.websocketTransport = response.transport;
+//            $rootScope.websocketRequest.sendMessage();
+//        };
+//
+//        $rootScope.websocketRequest.onClientTimeout = function (r) {
+//            $rootScope.websocketRequest.sendMessage();
+//            setTimeout(function () {
+//                $rootScope.websocketSubSocket = $rootScope.websocketSocket.subscribe($rootScope.websocketRequest);
+//            }, $rootScope.websocketRequest.reconnectInterval);
+//        };
+//
+//        $rootScope.websocketRequest.onClose = function (response) {
+//            if ($rootScope.websocketSubSocket) {
+//                $rootScope.websocketRequest.sendMessage();
+//            }
+//        };
+//
+//        $rootScope.websocketRequest.sendMessage = function () {
+//            if ($rootScope.websocketSubSocket.request.isOpen) {
+//                $rootScope.websocketSubSocket.push(atmosphere.util.stringifyJSON({
+//                        userLogin: $rootScope.login,
+//                        page: $route.current.templateUrl}
+//                ));
+//            }
+//        };
+//
+//        $rootScope.websocketSubSocket = $rootScope.websocketSocket.subscribe($rootScope.websocketRequest);
+//
+//        $rootScope.$on("$routeChangeSuccess", function (event, next, current) {
+//            $rootScope.websocketRequest.sendMessage();
+//        });
+//    })
+;
