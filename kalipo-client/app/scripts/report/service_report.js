27ae257e63ee32b7c3d04d8aaf06b3db376c5612
damoeb <maggo.ruepp@gmail.com>
2014-11-29 16:03:57 +0100

    moderation

--- kalipo-client/app/scripts/report/service_report.js
+++ kalipo-client/app/scripts/report/service_report.js
@@ -1,8 +1,10 @@
 'use strict';
 
 kalipoApp.factory('Report', function ($resource) {
-    return $resource('app/rest/reports/:id', {}, {
+    return $resource('app/rest/reports/:id/:filter', {}, {
         'pendingInThread': {method: 'GET', isArray: true, params: {id: 'pending'}},
+        'approve': {method: 'PUT', params: {id: '@id', filter: 'approve'}},
+        'reject': {method: 'PUT', params: {id: '@id', filter: 'reject'}},
         'query': { method: 'GET', isArray: true},
         'get': { method: 'GET'}
     });
