26c25e703eb89e2479b2bbcfe588fbc9caa86e32
damoeb <maggo.ruepp@gmail.com>
2015-02-27 01:14:14 +0100

    heise spider

--- kalipo-ir/harvester/spiders/heise_spider.py
+++ kalipo-ir/harvester/spiders/heise_spider.py
@@ -11,7 +11,7 @@ class HeiseSpider(CrawlSpider):
     name = "heise"
     allowed_domains = ["www.heise.de"]
     start_urls = [
-	#"http://www.heise.de/tp/forum",
+	"http://www.heise.de/tp/forum",
 	"http://www.heise.de/tp/foren/S-Syrien-150-Christen-von-IS-entfuehrt/forum-292707/list/"
     ]
 
@@ -23,13 +23,16 @@ class HeiseSpider(CrawlSpider):
     def clean_str(self, val):
 	return val.replace(u'\xa0', u'').replace('\n','').strip()
     
+    def to_str(self, arr):
+	return self.clean_str(''.join(arr))
+
     def parse_item(self, response):
         sel = Selector(response)
 
 	item = Comment()
-	item['text'] = sel.xpath('//posting_subject/text()').extract() + sel.xpath('//posting_text/html()').extract()
+	item['text'] = self.to_str(sel.xpath("//h3[@class='posting_subject']/text()").extract()) + self.to_str(sel.xpath("//p[@class='posting_text']/text()").extract())
 	item['url'] = response.url
-	item['author'] = sel.xpath('//user_info/i/text()').extract()
-	item['date'] = 'none' #calendar.timegm(time.strptime(self.clean_str(texts[1]).replace('Datum:',''), "%d-%m-%y %H:%M"))
+	item['author'] = self.to_str(sel.xpath("//div[@class='user_info']/i//text()").extract())
+	item['date'] = self.to_str(response.xpath("//div[@class='posting_date']/text()").extract()) #calendar.timegm(time.strptime(self.clean_str(texts[1]).replace('Datum:',''), "%d-%m-%y %H:%M"))
         return item
 
